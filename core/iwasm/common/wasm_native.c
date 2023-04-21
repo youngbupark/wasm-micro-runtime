5870ecbe18cac0553511c38831801ddce9c4b0c9
Young Bu Park <erviny.park@gmail.com>
2023-04-20 20:19:56 -0700

    fix build

--- core/iwasm/common/wasm_native.c
+++ core/iwasm/common/wasm_native.c
@@ -8,6 +8,7 @@
 #include "bh_log.h"
 
 #if !defined(BH_PLATFORM_ZEPHYR) && !defined(BH_PLATFORM_ALIOS_THINGS) \
+    && !defined(BH_PLATFORM_THREADX) \
     && !defined(BH_PLATFORM_OPENRTOS) && !defined(BH_PLATFORM_ESP_IDF)
 #define ENABLE_QUICKSORT 1
 #else
